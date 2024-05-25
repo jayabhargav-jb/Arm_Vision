ff4d85d2e59a57b34810ad391c3df9629af38272
Jaya Bhargav P <jayabhargav.jb@gmail.com>
2024-05-25 21:37:58 +0530

    modified mapping.py and added csv angle maps

--- mapping.py
+++ mapping.py
@@ -1,11 +1,17 @@
-import pandas
+import csv
+import numpy as np
 
-pandas_df_dict = pandas.read_excel("angle_map.xlsx", [0, 1, 2])
-print(pandas_df_dict[1])
-print(pandas_df_dict[1].interpolate(method="polynomial", order=5))
+with open("btm.csv", "r") as btm:
+    reader_obj = csv.reader(btm)
+    btm_arr_0 = list()
+    btm_arr_1 = list()
+    j = 0
+    for i in reader_obj:
+        if j != 0:
+            btm_arr_0.append(int(i[1]))
+            btm_arr_1.append(int(i[2]))
+        j = 1
 
-# line = str("")
-# line.find()
-# while line.find("Ready for input")>-1:
-#     pass
+best_fit_btm = np.polyfit(btm_arr_0, btm_arr_1, 5)
 
+print(type(best_fit_btm))
