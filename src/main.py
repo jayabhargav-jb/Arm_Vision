c3ade97a3270a3bcc4e808599edcbf07a0a1b79d
Ujwal N K <ujwal.nk@gmail.com>
2024-05-27 12:52:06 +0530

    Web Server

--- src/main.py
+++ src/main.py
@@ -1,19 +1,40 @@
-import img_proc
-import arm_kinematics
+from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+# import img_proc
+# import arm_kinematics
 import mapping
-import arduino_serial
+# import arduino_serial
 
-def main():
-    component_2D_coordinates = img_proc.proc_func()
-    joint_angles_in_deg = arm_kinematics.inverse_kinematics(component_2D_coordinates)
-    base_angle = joint_angles_in_deg[0]
-    btm_angle = mapping.map_btm(joint_angles_in_deg[1])
-    mid_angle = mapping.map_mid(joint_angles_in_deg[2])
-    top_angle = mapping.map_top(joint_angles_in_deg[3])
-    arduino_serial.pos(base_angle, btm_angle, mid_angle, top_angle)
-    arduino_serial.pick()
-    arduino_serial.drop()
-    return None
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+# To serve static files from static directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/res")
+async def pick_res():
+    # component_2D_coordinates = img_proc.proc_func(user_input="RESISTOR")
+    # joint_angles_in_deg = arm_kinematics.inverse_kinematics(component_2D_coordinates)
+    # base_angle = joint_angles_in_deg[0]
+    # btm_angle = mapping.map_btm(joint_angles_in_deg[1])
+    # mid_angle = mapping.map_mid(joint_angles_in_deg[2])
+    # top_angle = mapping.map_top(joint_angles_in_deg[3])
+    # arduinostatic_serial.pos(base_angle, btm_angle, mid_angle, top_angle)
+    # arduino_serial.pick()
+    # arduino_serial.drop()
+    print("RES")
+
+@app.get("/cap")
+async def pick_res():
+    # component_2D_coordinates = img_proc.proc_func(user_input="CAPACITOR")
+    # joint_angles_in_deg = arm_kinematics.inverse_kinematics(component_2D_coordinates)
+    # base_angle = joint_angles_in_deg[0]
+    # btm_angle = mapping.map_btm(joint_angles_in_deg[1])
+    # mid_angle = mapping.map_mid(joint_angles_in_deg[2])
+    # top_angle = mapping.map_top(joint_angles_in_deg[3])
+    # arduinostatic_serial.pos(base_angle, btm_angle, mid_angle, top_angle)
+    # arduino_serial.pick()
+    # arduino_serial.drop()
+    print("CAP")
